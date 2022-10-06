33b338ee3dece19c0a8d039d4b7855853b6c261c
lvshaoping007 <1244849816@qq.com>
2022-10-06 22:33:51 +0800

    add dapp connect empty view

--- src/popup/pages/AppConnection/index.js
+++ src/popup/pages/AppConnection/index.js
@@ -1,60 +1,83 @@
 import i18n from "i18next";
 import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { DAPP_CONNECTION_LIST, DAPP_DISCONNECT_SITE } from "../../../constant/types";
+import {
+  DAPP_CONNECTION_LIST,
+  DAPP_DISCONNECT_SITE,
+} from "../../../constant/types";
 import { sendMsg } from "../../../utils/commonMsg";
 import CustomView from "../../component/CustomView";
 import Toast from "../../component/Toast";
 import styles from "./index.module.scss";
 
-const AppConnection = ({ }) => {
+const AppConnection = ({}) => {
+  const currentAddress = useSelector(
+    (state) => state.accountInfo.currentAccount.address
+  );
 
-  const currentAddress = useSelector(state => state.accountInfo.currentAccount.address)
-
-  const [connectList, setConnectList] = useState([])
+  const [connectList, setConnectList] = useState([]);
 
   useEffect(() => {
-    sendMsg({
-      action: DAPP_CONNECTION_LIST,
-      payload: {
-        address: currentAddress
+    sendMsg(
+      {
+        action: DAPP_CONNECTION_LIST,
+        payload: {
+          address: currentAddress,
+        },
+      },
+      (list) => {
+        setConnectList(list);
       }
-    }, (list) => {
-      setConnectList(list)
-    })
-  }, [])
+    );
+  }, []);
 
-
-  const onDeleteConnect = useCallback((item, index) => {
-    sendMsg({
-      action: DAPP_DISCONNECT_SITE,
-      payload: {
-        siteUrl: item,
-        address: currentAddress
-      }
-    }, (status) => {
-      if (status) {
-        let newList = [...connectList]
-        newList.splice(index, 1)
-        setConnectList(newList)
-      } else {
-        Toast.info(i18n.t('disconnectFailed'))
-      }
-    })
-  }, [connectList, i18n, currentAddress])
-
+  const onDeleteConnect = useCallback(
+    (item, index) => {
+      sendMsg(
+        {
+          action: DAPP_DISCONNECT_SITE,
+          payload: {
+            siteUrl: item,
+            address: currentAddress,
+          },
+        },
+        (status) => {
+          if (status) {
+            let newList = [...connectList];
+            newList.splice(index, 1);
+            setConnectList(newList);
+          } else {
+            Toast.info(i18n.t("disconnectFailed"));
+          }
+        }
+      );
+    },
+    [connectList, i18n, currentAddress]
+  );
 
   return (
-    <CustomView title={i18n.t("appConnection")} >
-      {
-        connectList.map((item, index) => {
-          return (<div className={styles.rowContainer} key={index}>
-            <span>{item}</span>
-            <img src="/img/icon_delete.svg" onClick={() => onDeleteConnect(item, index)} />
-          </div>)
-        })
-      }
+    <CustomView title={i18n.t("appConnection")}>
+      {connectList.length === 0 ? (
+        <div className={styles.emptyContainer}>
+          <img src="/img/icon_empty.svg" className={styles.emptyIcon} />
+          <p className={styles.noDAppTip}>{i18n.t("noConnectedApps")}</p>
+        </div>
+      ) : (
+        <>
+          {connectList.map((item, index) => {
+            return (
+              <div className={styles.rowContainer} key={index}>
+                <span>{item}</span>
+                <img
+                  src="/img/icon_delete.svg"
+                  onClick={() => onDeleteConnect(item, index)}
+                />
+              </div>
+            );
+          })}
+        </>
+      )}
     </CustomView>
-  )
-}
-export default AppConnection
+  );
+};
+export default AppConnection;
