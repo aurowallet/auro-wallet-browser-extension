aa30c1e0f308593c5bd27814cf2f5b75af18c676
wjdfx <wjdfxa@gmail.com>
2024-01-16 19:10:55 +0800

    Update WelcomePage ui

--- src/popup/pages/Welcome/index.js
+++ src/popup/pages/Welcome/index.js
@@ -11,11 +11,79 @@ import { setWelcomeNextRoute } from "../../../reducers/cache";
 import { openTab } from "../../../utils/commonMsg";
 import Button, { button_theme } from "../../component/Button";
 import { PopupModal } from "../../component/PopupModal";
-import styles from "./index.module.scss";
 import extension from "extensionizer";
+import styled from "styled-components";
 
 const type_conditions = "conditions";
 const type_policy = "policy";
+
+const StyledOuterWrapper = styled.div`
+  height: 100vh;
+  min-width: 750px;
+  min-height: 600px;
+  background-color: rgb(249, 250, 252);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledInnerContent = styled.div`
+  background-color: white;
+  max-width: 750px;
+  max-height: 600px;
+`;
+const StyledTopContainer = styled.div`
+  width: 375px;
+  margin: 0 auto;
+`;
+const StyledLogoContainer = styled.div`
+  margin: 89px auto 57px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+const StyledBtnContainer = styled.div`
+  padding: 0px 38px;
+  > :not(:last-child) {
+    margin-bottom: 20px;
+  }
+`;
+const StyledLedgerTip = styled.p`
+  margin: 30px auto 16px;
+  font-size: 12px;
+  text-align: center;
+
+  color: #cccccc;
+  padding: 0px 20px;
+`;
+
+const StyledPowerBy = styled.p`
+  font-size: 12px;
+  line-height: 18px;
+  color: var(--nobelGray);
+  text-align: center;
+  margin: 0 auto;
+`;
+const StyledModalWrapper = styled.div`
+  > :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+const StyledModalContent = styled.p`
+  font-size: 14px;
+  line-height: 17px;
+  color: var(--mediumBlack);
+`;
+
+const StyledModalTip = styled.span`
+  color: var(--mainBlue);
+  &:hover {
+    color: linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)),
+      var(--mainBlue);
+  }
+  cursor: pointer;
+`;
 
 const Welcome = () => {
   const cache = useSelector((state) => state.cache);
@@ -96,76 +164,76 @@ const Welcome = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.topContainer}>
-        <div className={styles.logoContainer}>
-          <img src="/img/colorful_logo.svg" className={styles.logo} />
-        </div>
+    <StyledOuterWrapper>
+      <StyledInnerContent>
+        <StyledTopContainer>
+          <StyledLogoContainer>
+            <img src="/img/colorful_logo.svg" />
+          </StyledLogoContainer>
 
-        <div className={styles.btnContainer}>
-          <Button
-            leftIcon={"/img/icon_add.svg"}
-            onClick={() => {
-              // goNextRoute("/backup_tips")
-              openFullClick();
-            }}
-          >
-            {i18n.t("createWallet")}
-          </Button>
+          <StyledBtnContainer>
+            <Button
+              leftIcon={"/img/icon_add.svg"}
+              onClick={() => {
+                // goNextRoute("/backup_tips")
+                openFullClick();
+              }}
+            >
+              {i18n.t("createWallet")}
+            </Button>
 
-          <Button
-            theme={button_theme.BUTTON_THEME_LIGHT}
-            leftIcon={"/img/icon_download.svg"}
-            onClick={() => {
-              goNextRoute("/restore_account");
-            }}
-          >
-            {i18n.t("restoreWallet")}
-          </Button>
-        </div>
-      </div>
-      <p className={styles.bottomTipLedger}>{i18n.t("ledgerUserTip")}</p>
-      <p className={styles.bottomUrl}>{POWER_BY}</p>
+            <Button
+              theme={button_theme.BUTTON_THEME_LIGHT}
+              leftIcon={"/img/icon_download.svg"}
+              onClick={() => {
+                goNextRoute("/restore_account");
+              }}
+            >
+              {i18n.t("restoreWallet")}
+            </Button>
+          </StyledBtnContainer>
+        </StyledTopContainer>
+        <StyledLedgerTip>{i18n.t("ledgerUserTip")}</StyledLedgerTip>
+        <StyledPowerBy>{POWER_BY}</StyledPowerBy>
 
-      <PopupModal
-        title={i18n.t("termsAndPrivacy")}
-        leftBtnContent={i18n.t("refuse")}
-        rightBtnContent={i18n.t("agree")}
-        onLeftBtnClick={onCloseModal}
-        onRightBtnClick={() => {
-          onCloseModal();
-          goPage(nextRoute, "saveProtocol");
-        }}
-        componentContent={
-          <div>
-            <p className={styles.confirmContent_1}>
-              {i18n.t("termsAndPrivacy_line1")}
-            </p>
-            <p className={styles.confirmContent_2}>
-              <Trans
-                i18nKey={i18n.t("termsAndPrivacy_line2")}
-                components={{
-                  conditions: (
-                    <span
-                      className={styles.tipsSpical}
-                      onClick={() => onClickGuide(type_conditions)}
-                    />
-                  ),
-                  policy: (
-                    <span
-                      className={styles.tipsSpical}
-                      onClick={() => onClickGuide(type_policy)}
-                    />
-                  ),
-                }}
-              />
-            </p>
-          </div>
-        }
-        modalVisable={popupModalStatus}
-        onCloseModal={onCloseModal}
-      />
-    </div>
+        <PopupModal
+          title={i18n.t("termsAndPrivacy")}
+          leftBtnContent={i18n.t("refuse")}
+          rightBtnContent={i18n.t("agree")}
+          onLeftBtnClick={onCloseModal}
+          onRightBtnClick={() => {
+            onCloseModal();
+            goPage(nextRoute, "saveProtocol");
+          }}
+          componentContent={
+            <StyledModalWrapper>
+              <StyledModalContent>
+                {i18n.t("termsAndPrivacy_line1")}
+              </StyledModalContent>
+              <StyledModalContent>
+                <Trans
+                  i18nKey={i18n.t("termsAndPrivacy_line2")}
+                  components={{
+                    conditions: (
+                      <StyledModalTip
+                        onClick={() => onClickGuide(type_conditions)}
+                      />
+                    ),
+                    policy: (
+                      <StyledModalTip
+                        onClick={() => onClickGuide(type_policy)}
+                      />
+                    ),
+                  }}
+                />
+              </StyledModalContent>
+            </StyledModalWrapper>
+          }
+          modalVisable={popupModalStatus}
+          onCloseModal={onCloseModal}
+        />
+      </StyledInnerContent>
+    </StyledOuterWrapper>
   );
 };
 export default Welcome;
