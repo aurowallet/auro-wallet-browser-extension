3c4f05a0ff096c26a65a3fcedc4f0cc6e2f36e4c
wjdfx <wjdfxa@gmail.com>
2024-05-11 11:26:42 +0800

    Update: add token img source

---
+++ src/popup/component/SVG/icon_add.js
@@ -0,0 +1,19 @@
+function IconAdd({ fill }) {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15 23C10.5817 23 7 19.4183 7 15C7 10.5817 10.5817 7 15 7C19.4183 7 23 10.5817 23 15C23 19.4183 19.4183 23 15 23ZM5 15C5 9.47715 9.47715 5 15 5C20.5228 5 25 9.47715 25 15C25 20.5228 20.5228 25 15 25C9.47715 25 5 20.5228 5 15ZM14 19V16H11V14H14V11H16V14H19V16H16V19H14Z"
+        fill={fill}
+      />
+    </svg>
+  );
+}
+export default IconAdd;
